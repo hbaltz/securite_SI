12ea077f7b87f751b69b573c714c93bcd07b39e1
hblatz <hugo.baltz@ensg.eu>
2016-11-03 14:12:26 +0100

    Restriction sur l'upload

--- blog/application/helpers/upload_helper.php
+++ blog/application/helpers/upload_helper.php
@@ -10,6 +10,15 @@ if ( !function_exists('custom_upload') )
         // Configuration pour l'upload de l'image
         $config['upload_path'] = 'uploaded_files/';
         $config['allowed_types'] = 'gif|jpg|png';
+        //$config['encrypt_name'] = 'TRUE'; // On le re fait à la main en dessous
+        $config['max_filename'] = '21';
+        $config['max_size'] = '2048';
+        $config['max_width'] = '1300';
+        $config['max_height'] = '800';
+
+        $ext = end(explode(".",$_FILES['image']['name']));
+        $CI->load->helper('string');
+        $config['file_name'] = $data['file_name'] = random_string('alnum',20).".".$ext;
 
         $CI->load->library('upload', $config);
         $data['error'] = FALSE;
@@ -18,7 +27,7 @@ if ( !function_exists('custom_upload') )
         if ( ! $CI->upload->do_upload('image')) :
             $data['error'] = $CI->upload->display_errors('', '');
         else:
-            $data['file_name'] = $_FILES['image']['name'];
+            //$data['file_name'] = $_FILES['image']['name'];
         endif;
 
         return $data;
