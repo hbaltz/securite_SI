3da714e1bbe3ac1452c2346b55fe6fee2e8ca664
hblatz <hugo.baltz@ensg.eu>
2016-11-03 12:20:26 +0100

    Correction ajout fichier

--- blog/application/helpers/upload_helper.php
+++ blog/application/helpers/upload_helper.php
@@ -9,7 +9,7 @@ if ( !function_exists('custom_upload') )
 
         // Configuration pour l'upload de l'image
         $config['upload_path'] = 'uploaded_files/';
-	$config['allowed_types'] = '*';
+        $config['allowed_types'] = 'gif|jpg|png';
 
         $CI->load->library('upload', $config);
         $data['error'] = FALSE;
@@ -17,7 +17,7 @@ if ( !function_exists('custom_upload') )
         // Si l'upload a échoué, on l'affiche
         if ( ! $CI->upload->do_upload('image')) :
             $data['error'] = $CI->upload->display_errors('', '');
-	else:
+        else:
             $data['file_name'] = $_FILES['image']['name'];
         endif;
 
