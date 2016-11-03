25b21ca267955dd94d62b14def556eadb809b0d8
hblatz <hugo.baltz@ensg.eu>
2016-11-03 15:39:20 +0100

    Resize image

--- blog/application/helpers/upload_helper.php
+++ blog/application/helpers/upload_helper.php
@@ -8,14 +8,16 @@ if ( !function_exists('custom_upload') )
         $CI=& get_instance();
 
         // Configuration pour l'upload de l'image
+        $config['image_library'] = 'gd2';
         $config['upload_path'] = 'uploaded_files/';
-        $config['allowed_types'] = 'gif|jpg|png';
-        //$config['encrypt_name'] = 'TRUE'; // On le re fait à la main en dessous
+        $config['allowed_types'] = 'gif|jpg|png|jpeg';
+        //$config['encrypt_name'] = 'TRUE'; // On le refait à la main en dessous
         $config['max_filename'] = '21';
         $config['max_size'] = '2048';
         $config['max_width'] = '1300';
         $config['max_height'] = '800';
 
+        // On encrypte le nom de l'image
         $ext = end(explode(".",$_FILES['image']['name']));
         $CI->load->helper('string');
         $config['file_name'] = $data['file_name'] = random_string('alnum',20).".".$ext;
@@ -28,6 +30,27 @@ if ( !function_exists('custom_upload') )
             $data['error'] = $CI->upload->display_errors('', '');
         else:
             //$data['file_name'] = $_FILES['image']['name'];
+
+            // On redimensionne l'image : 
+            $CI->load->library('image_lib');
+            $config['source_image'] = $CI->upload->upload_path.$config['file_name'];
+
+            $sizes = array(array(270, 220), array(770,300));
+            foreach ($sizes as $size) {
+                $config['maintain_ratio'] = false;
+                $config['width'] = $size[0];
+                $config['height'] = $size[1];
+                $config['new_image'] = $config['upload_path'].$size[0]."_".$size[1]."_".$config['file_name'];
+
+                $CI->image_lib->clear();
+                $CI->image_lib->initialize($config);
+                $CI->image_lib->resize();
+
+                if( ! $CI->image_lib->resize()){
+                    $data['error'] = $CI->image_lib->display_errors('', '');
+                }
+            }
+
         endif;
 
         return $data;
